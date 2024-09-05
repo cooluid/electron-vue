d8bb8d356b19d0274ee93a6ffcb01c1ae4c57e10
cooluid <400822@163.com>
2024-09-05 22:35:15 +0800

    结构修改

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+
+createApp(App).use(pinia).use(ElementPlus).mount('#app')
