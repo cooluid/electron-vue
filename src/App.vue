e73b910bf8fad0b1cf0513b382c048e5c43b1c82
cooluid <400822@163.com>
2024-09-06 17:32:25 +0800

    基本布局调整，防止出现滚动条：overflow:hidden

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <script setup lang="ts">
-import { useDark } from '@vueuse/core';
-import { watch } from 'vue';
+import { useDark } from "@vueuse/core";
+import { watch } from "vue";
 import Home from "./views/Home.vue";
 
 const isDark = useDark();
 
-watch(isDark, (dark) => {
-    console.log(`isDark`, dark);
-    if (dark) {
-        document.documentElement.classList.add('dark');
-    } else {
-        document.documentElement.classList.remove('dark');
-    }
-}, { immediate: true });
+watch(
+	isDark,
+	(dark) => {
+		console.log(`isDark`, dark);
+		if (dark) {
+			document.documentElement.classList.add("dark");
+		} else {
+			document.documentElement.classList.remove("dark");
+		}
+	},
+	{ immediate: true }
+);
 </script>
 
 <template>
-    <div id="app-container">
-        <Home />
-    </div>
+	<div id="app-container">
+		<Home />
+	</div>
 </template>
 
 <style scoped>
 #app-container {
-    width: 100vw;
-    height: 100vh;
-    box-sizing: border-box;
+	width: 100vw;
+	height: 100vh;
+	box-sizing: border-box;
 }
 </style>
--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,61 +1,69 @@
 <template>
-    <div class="header">
-        <div class="title-bar">
-            <div class="drag-area" />
-            <IconItem icon="#icon-guanbi" :size="16" @click="closeApp" class="no-drag close-btn" />
-        </div>
-        <div class="icon-bar">
-            <IconItem icon="#icon-shezhi" :size="20" class="no-drag" />
-        </div>
-    </div>
+	<div class="header">
+		<div class="title-bar">
+			<div class="drag-area" />
+			<IconItem
+				icon="#icon-guanbi"
+				:size="16"
+				@click="closeApp"
+				class="no-drag close-btn"
+			/>
+		</div>
+		<div class="icon-bar">
+			<IconItem icon="#icon-shezhi" :size="20" />
+			<IconItem icon="#icon-a-zu4" :size="18" />
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
-import IconItem from '@/components/com/IconItem.vue'
+import IconItem from "@/components/com/IconItem.vue";
 
 const closeApp = () => {
-    window.electronAPI.invoke('close-window');
+	window.electronAPI.invoke("close-window");
 };
 </script>
 
 <style scoped>
 .header {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+	width: 100%;
+	height: 60px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	overflow: hidden;
 }
 
 .title-bar {
-    width: 100%;
-    padding: 5px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    -webkit-app-region: drag;
-    background-color: #000;
+	width: 100%;
+	flex: 1;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	-webkit-app-region: drag;
 }
 
 .close-btn {
-    margin-right: 10px;
+	margin-right: 10px;
 }
 
 .drag-area {
-    flex-grow: 1;
-    height: 100%;
+	flex-grow: 1;
+	height: 100%;
 }
 
 .no-drag {
-    -webkit-app-region: no-drag;
+	-webkit-app-region: no-drag;
 }
 
 .icon-bar {
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-    padding:10px 0 5px 20px;
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-start;
+	align-items: center;
+	gap: 5px;
+	padding: 5px 0 5px 10px;
 }
 </style>
